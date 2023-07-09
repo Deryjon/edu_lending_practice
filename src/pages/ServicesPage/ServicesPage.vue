22cd52c7369f8b986536b40e5425ac37de40673e
Dery <yusupoviskandar@gmail.com>
2023-07-09 15:49:25 +0500

    feat: finished services page (2023-07-09, 15:49)

--- src/pages/ServicesPage/ServicesPage.vue
+++ src/pages/ServicesPage/ServicesPage.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="services-page min-h-screen">
     <ServiceSection />
+
+    <ConnectWithComponents />
+    <QuestionComponent />
   </div>
 </template>
 <script>
 import ServiceSection from "./sections/ServiceSection.vue";
+import ConnectWithComponents from "../../components/ConnectWithComponent.vue";
+import QuestionComponent from "../../components/QuestionComponent.vue";
 export default {
-  components: { ServiceSection },
+  components: { ServiceSection, ConnectWithComponents, QuestionComponent },
 };
 </script>
