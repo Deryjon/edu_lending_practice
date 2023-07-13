1438c3734cf5843d2f9429429701d7481153c84e
Dery <yusupoviskandar@gmail.com>
2023-07-13 12:47:52 +0500

    fix: fixed width header btn (2023-07-13, 12:47)

--- src/layouts/HeaderLayout/components/HeaderActions.vue
+++ src/layouts/HeaderLayout/components/HeaderActions.vue
@@ -3,7 +3,7 @@
       <div class="img w-[38px] h-[38px] rounded-3xl m-[5px] flex items-center">
         <img src="../../../assets/icons/chat.svg " class="p-3" alt="" />
       </div>
-      <p class="flex py-3 ml-11 lg:ml-[16px] text-brand text-sm font-bold lg:text-[16px]">Оставить заявку</p>
+      <p class="flex py-3 ml-8 text-brand text-sm font-bold lg:text-[16px]">Оставить заявку</p>
     </button>
     <button class="burger-btn w-9 h-9 mt-3 ml-[65px] flex justify-center items-center lg:hidden order-3" @click="$emit('openMenu')" >
       <img src="../../../assets/icons/burger.svg" alt="burger-btn">
