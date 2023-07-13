0ec3a4fd023ced4ec0b3d20b93e91826f569cba3
lonewolf0844 <lonewolf.6120844@gmail.com>
2023-07-13 14:39:48 +0500

    feat: finish news page (2023-07-13, 14:39)

--- src/pages/NewsPage/NewsPage.vue
+++ src/pages/NewsPage/NewsPage.vue
@@ -2,10 +2,13 @@
 
   <div class="news-page min-h-screen">
     <NewsSection/>
+		<ConnectWithComponent/>
+		<QuestionComponent/>
   </div>
 </template>
 <script>
 import NewsSection from './sections/NewsSection.vue';
-
-export default { components: { NewsSection } };
+import ConnectWithComponent from '../../components/ConnectWithComponent.vue'
+import QuestionComponent from '../../components/QuestionComponent.vue'
+export default { components: { NewsSection, ConnectWithComponent, QuestionComponent } };
 </script>
--- src/pages/NewsPage/components/CardNewsComponents.vue
+++ src/pages/NewsPage/components/CardNewsComponents.vue
@@ -1,35 +1,39 @@
 <template>
   <v-card
-    class="card lg:flex lg:gap-5 card w-[300px] lg:w-[850px] h-[390px] lg:h-[200px] bg-[rgba(255, 255, 255, 1)] mt-6 mx-auto rounded-[20px] p-[9px] lg:p-[12px]"
+    class="card lg:flex lg:gap-5 card w-[300px] lg:w-[850px] h-[390px] lg:h-[200px] bg-[rgba(255, 255, 255, 1)] mt-6 mx-auto rounded-[20px] p-[9px] lg:p-[12px] cursor-pointer"
     v-for="(commentIndex, index) in commentsToShow"
     :key="index"
-    v-show="index < studentsData.length"
+    v-show="index < newsData.length"
   >
     <v-img class="card-img w-[282px] h-[182px]  rounded-[20px]">
       <img
-        :src="studentsData[index].image"
+        :src="newsData[index].image"
         alt="card-img-student"
         class="w-full h-full"
       />
     </v-img>
     <div class="text">
       <p class="text-gray font-bold text-sm lg:text-lg mt-[10px]">
-        {{ studentsData[index].name }} {{ index + 1 }}
+        {{ newsData[index].name }} {{ index + 1 }}
       </p>
       <p class="text-[10px] lg:text-xs text-gray mt-1 lg:mt-[6px]">
-        {{ studentsData[index].description }}
+        {{ newsData[index].description }}
       </p>
       <div class="university flex gap-2 mt-[6px] lg:mt-[10px]">
         <div class="icon">
-          <img src="../../../assets/icons/university-icon.svg" alt="" />
+          <img src="../../../assets/icons/time-icon-news.svg" alt="" />
         </div>
         <p class="text-gray text-[10px] lg:text-xs">
-          {{ studentsData[index].university }}
+					Время чтения:
+					<span class="font-bold">
+
+						{{ newsData[index].university }}
+					</span>
         </p>
       </div>
     </div>
   </v-card>
-  <div class="action" v-show="commentsToShow < studentsData.length">
+  <div class="action" v-show="commentsToShow < newsData.length">
     <theMoreButton
       @click="commentsToShow += 3"
       class="mx-auto mt-[20px] lg:mt-[30px]"
@@ -44,130 +48,123 @@ export default {
     data: () => ({
     totalComments: 0,
     commentsToShow: 3,
-    studentsData: [
+    newsData: [
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         university: "10 минут",
       },
       {
         image: "src/assets/images/red-square.jpg",
         name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit...;",
         time: "10 минут",
       },
     ],
   }),
   methods: {
     // loadMore() {
-    //   // const newItems = Array.from({ length: this.pageSize }, () => ({
-    //   //   image: "src/assets/icons/man-students.svg",
-    //   //   name: "Александр Иванов",
-    //   //   description:
-    //   //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. Lorem ipsum dolor sit amet, consectetur adipiscing elit! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!",
-    //   //   university: "Название университета",
-    //   // }));
     //   // this.studentsData = this.studentsData.concat(newItems);
     //   // this.currentPage++;
     // },
   },
   mounted() {
-    this.totalComments = this.studentsData.length;
-    console.log(this.studentsData.length);
+    this.totalComments = this.newsData.length;
+    console.log(this.newsData.length);
   },
 };
 </script>
